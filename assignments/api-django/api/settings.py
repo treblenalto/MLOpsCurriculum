93dcc503fca03287dc3435e72f8506fa3858443e
Taehee-K <taehee107275@gmail.com>
2022-03-16 21:49:35 +0900

    chore: hide database info

--- assignments/api-django/api/settings.py
+++ assignments/api-django/api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from secret import SECRET_KEY, DEFAULT_DATABASE
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = BASE_DIR / "secret_key.json"
+SECRET_KEY = BASE_DIR / SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
@@ -81,16 +82,7 @@ WSGI_APPLICATION = "api.wsgi.application"
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "test",
-        "USER": "opsnovice",
-        "PASSWORD": "mlops",
-        "HOST": "localhost",
-        "PORT": "",
-    }
-}
+DATABASES = DEFAULT_DATABASE
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
