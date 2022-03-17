fa91c1f2f046388c9ef05ea4d82d78df1c95a90a
Taehee-K <taehee107275@gmail.com>
2022-03-17 12:38:32 +0900

    feat: response messages for post, put, delete

--- assignments/api-django/user/serializers.py
+++ assignments/api-django/user/serializers.py
@@ -6,3 +6,13 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User  # select model
         fields = ("id", "name", "age")  # select fields
+    
+    def validate_name(self, instance):
+        if instance.replace(" ", "").encode().isalpha()==False:
+            raise serializers.ValidationError("Name must contain only alphabet letters")
+        return instance.strip()
+        
+    def validate_age(self, instance):
+        if instance < 0:
+            raise serializers.ValidationError("Age must be positive")
+        return instance
--- assignments/api-django/user/views.py
+++ assignments/api-django/user/views.py
@@ -1,5 +1,6 @@
 # from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.response import Response
 from .serializers import UserSerializer
 from .models import User
 
@@ -7,3 +8,26 @@ from .models import User
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()  # SELECT * FROM User
     serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=False):
+            serializer.save()
+            return Response({"success": True, "message": "User created successfully"})
+        else:
+            return Response({"success": False, "message": serializer.errors})
+
+    def update(self, request, *args, **kwargs):
+        objects = User.objects.get(id=kwargs["pk"])
+        serializer = UserSerializer(objects, data=request.data)
+        if serializer.is_valid(raise_exception=False):
+            serializer.save()
+            return Response({"success": True, "message": "User updated successfully"})
+        else:
+            return Response({"success": False, "message": serializer.errors})
+
+    def destroy(self, request, *args, **kwargs):
+        objects = User.objects.get(id=kwargs["pk"])
+        objects.delete()
+        return Response({"success": True, "message": "User deleted successfully"})
+
