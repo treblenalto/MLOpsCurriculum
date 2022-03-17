bfb61a4d8dff56c4806cb1ca4e4eee8ede3083dd
Taehee-K <taehee107275@gmail.com>
2022-03-17 13:52:26 +0900

    feat: return http status code

--- assignments/api-django/user/views.py
+++ assignments/api-django/user/views.py
@@ -1,5 +1,6 @@
 # from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework import status
 from rest_framework.response import Response
 from .serializers import UserSerializer
 from .models import User
@@ -13,21 +14,20 @@ class UserViewSet(viewsets.ModelViewSet):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid(raise_exception=False):
             serializer.save()
-            return Response({"success": True, "message": "User created successfully"})
+            return Response({"success": True, "message": "User created successfully"}, status=status.HTTP_201_CREATED)
         else:
-            return Response({"success": False, "message": serializer.errors})
+            return Response({"success": False, "message": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
     def update(self, request, *args, **kwargs):
         objects = User.objects.get(id=kwargs["pk"])
         serializer = UserSerializer(objects, data=request.data)
         if serializer.is_valid(raise_exception=False):
             serializer.save()
-            return Response({"success": True, "message": "User updated successfully"})
+            return Response({"success": True, "message": "User updated successfully"}, status=status.HTTP_200_OK)
         else:
-            return Response({"success": False, "message": serializer.errors})
+            return Response({"success": False, "message": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
     def destroy(self, request, *args, **kwargs):
         objects = User.objects.get(id=kwargs["pk"])
         objects.delete()
-        return Response({"success": True, "message": "User deleted successfully"})
-
+        return Response({"success": True, "message": "User deleted successfully"}, status=status.HTTP_200_OK)
