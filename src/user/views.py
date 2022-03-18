97a4829b6570f36e581c65120a6c5e093ac8ebc5
Taehee-K <taehee107275@gmail.com>
2022-03-18 22:11:10 +0900

    fix: return deleted user info

--- src/user/views.py
+++ src/user/views.py
@@ -9,3 +9,10 @@ from .models import User
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()  # SELECT * FROM User
     serializer_class = UserSerializer
+    
+    # return id, name, age of deleted user
+    def destroy(self, request, *args, **kwargs):
+        objects = User.objects.get(id=kwargs["pk"])
+        serialized = UserSerializer(objects).data
+        objects.delete()
+        return Response(serialized)
