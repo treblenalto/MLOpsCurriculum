7cecb4dbe813824940518f173bf3ebf3724a9dd5
Taehee-K <taehee107275@gmail.com>
2022-03-29 17:40:33 +0900

    fix: remove invalid http host header

--- src/api/settings.py
+++ src/api/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = BASE_DIR / os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1", "localhost", "postgres"]
+ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1", "localhost"]
 
 
 # Application definition
