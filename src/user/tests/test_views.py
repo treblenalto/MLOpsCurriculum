02f236e18e2133b283703f11a02df25754b127b6
Taehee-K <taehee107275@gmail.com>
2022-05-05 01:04:58 +0900

    fix: test only methods created by me

--- src/user/tests/test_views.py
+++ src/user/tests/test_views.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse_lazy
-from rest_framework import serializers
+from rest_framework import serializers, exceptions
 from rest_framework.test import APIRequestFactory
 
 from user.models import User
@@ -8,47 +8,16 @@ from user.views import UserViewSet
 from user.serializers import UserSerializer
 
 
-class UserTestCase(TestCase):
+class UserViewTestCase(TestCase):
     def setUp(self):
         User.objects.create(name="Testzero", age=20)
         User.objects.create(name="Testone", age=21)
 
-    # get all users
-    def test_get_user_list(self):
-        request = APIRequestFactory().get(reverse_lazy("user-list"))
-        view = UserViewSet.as_view({"get": "list"})
-        response = view(request)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.data), 2)
-        
-    # get a user
-    def test_get_user_detail(self):
-        Testzero = User.objects.get(name="Testzero")
-        request = APIRequestFactory().get(reverse_lazy("user-detail", args=[Testzero.id]))
-        view = UserViewSet.as_view({"get": "retrieve"})
-        response = view(request, pk=Testzero.id)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data["name"], "Testzero")
-        self.assertEqual(response.data["age"], 20)
-
-    # get a user with invalid id
-    def test_get_user_detail_invalid_id(self):
-        request = APIRequestFactory().get(reverse_lazy("user-detail", args=[-1]))
-        view = UserViewSet.as_view({"get": "retrieve"})
-        response = view(request, pk=-1)
-        self.assertEqual(response.status_code, 404)
-        
-    # create a user
-    def test_post_user(self):
-        request = APIRequestFactory().post(reverse_lazy("user-list"), {"name": "Testtwo", "age": 23})
-        view = UserViewSet.as_view({"post": "create"})
-        response = view(request)
-        self.assertEqual(response.status_code, 201)
-        self.assertEqual(User.objects.get(name="Testtwo").age, 23)
-
     # create a user with no age - nullable
     def test_post_user_no_age(self):
-        request = APIRequestFactory().post(reverse_lazy("user-list"), {"name": "Testthree"})
+        request = APIRequestFactory().post(
+            reverse_lazy("user-list"), {"name": "Testthree"}
+        )
         view = UserViewSet.as_view({"post": "create"})
         response = view(request)
         self.assertEqual(response.status_code, 201)
@@ -61,7 +30,7 @@ class UserTestCase(TestCase):
         ):
             serializer = UserSerializer(data={"name": "Testfive", "age": -1})
             serializer.is_valid(raise_exception=True)
-            
+
     # create a user with no name
     def test_post_user_no_name(self):
         with self.assertRaisesMessage(
@@ -69,33 +38,13 @@ class UserTestCase(TestCase):
         ):
             serializer = UserSerializer(data={"age": 23})
             serializer.is_valid(raise_exception=True)
-            
-    # update a user
-    def test_put_user(self):
-        Testone = User.objects.get(name="Testone")
-        request = APIRequestFactory().put(reverse_lazy("user-detail", args=[Testone.id]), {"name": "Testone", "age": 30})
-        view = UserViewSet.as_view({"put": "update"})
-        response = view(request, pk=Testone.id)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(User.objects.get(name="Testone").age, 30)
 
-    # update a user with invalid id 
-    def test_put_user_invalid_id(self):
-        request = APIRequestFactory().put(reverse_lazy("user-detail", args=[-1]), {"name": "Testone", "age": 30})
-        view = UserViewSet.as_view({"put": "update"})
-        response = view(request, pk=-1)
-        self.assertEqual(response.status_code, 404)
-        
     # delete a user
     def test_delete_user(self):
         Testone = User.objects.get(name="Testone")
-        request = APIRequestFactory().delete(reverse_lazy("user-detail", args=[Testone.id]))
+        request = APIRequestFactory().delete(
+            reverse_lazy("user-detail", args=[Testone.id])
+        )
         view = UserViewSet.as_view({"delete": "destroy"})
         response = view(request, pk=Testone.id)
         self.assertEqual(response.status_code, 200)
-    
-    def test_delete_user_invalid_id(self):
-        request = APIRequestFactory().delete(reverse_lazy("user-detail", args=[-1]))
-        view = UserViewSet.as_view({"delete": "destroy"})
-        response = view(request, pk=-1)
-        self.assertEqual(response.status_code, 404)
